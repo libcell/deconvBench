9b9088478531e91130c5045ffe729d23eafc07c3
Bo Lee <libcell@cqu.edu.cn>
2021-11-02 21:39:29 +0800

    added new codes

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("######################################################################
+Welcome to 'deconvBench' package
+Find citation details with citation(\"deconvBench\")
+Use SomEnvGUI() to start the Graphical User Interface in the default browser
+For further information see the scrolling Help in the Graphical User Interface
+######################################################################
+")
+}
