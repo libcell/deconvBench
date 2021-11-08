60a65d04c109a4afaadaa62baad01da80eca3a5b
Bo Lee <libcell@cqu.edu.cn>
2021-11-08 22:10:39 +0800

    Update ADAPTS.R

--- R/ADAPTS.R
+++ R/ADAPTS.R
@@ -8,8 +8,9 @@ Adapts <- function(bulkdata, signature = LM22, method = "nnls") {
   if (method == "SVMDECON") {
     bulkdata <- bulkdata[apply(bulkdata, 1, var) != 0, ]
   }
-  res <- estCellPercent(refExpr = signature,
+  res <- ADAPTS::estCellPercent(refExpr = signature,
                         geneExpr = bulkdata,
                         method = method)
   return(res)
 }
+
