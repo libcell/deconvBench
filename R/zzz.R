23d355cb45525f8d9ad65ec77c74db20dc302a33
Bo Lee <libcell@cqu.edu.cn>
2021-11-21 22:58:30 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
   packageStartupMessage("######################################################################
 Welcome to 'deconvBench' package
 Find citation details with citation(\"deconvBench\")
-Use SomEnvGUI() to start the Graphical User Interface in the default browser
-For further information see the scrolling Help in the Graphical User Interface
+Use deconvEnvGUI() to start the Graphical User Interface in default browser
+For detailed information see the scrolling Help in Graphical User Interface
 ######################################################################
 ")
 }
